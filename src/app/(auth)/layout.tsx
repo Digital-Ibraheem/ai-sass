5ba9eb98e8c504217d04217a356456e6ac5b0bd0
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-10-05 19:38:23 +0600

    ci: clerk auth setup

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => (
+  <div className='flex items-center h-full justify-center'>{children}</div>
+);
+
+export default AuthLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,8 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
